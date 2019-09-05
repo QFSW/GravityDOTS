3c710bc4d6bd2e939c94f46499cff5427cb74bf0
Miren <mirenbhakta21@gmail.com>
2019-09-05 17:43:49 -0500

    uhhhhh yes more from last time aaaaaaa

--- Assets/Scripts/Mono Behaviours/ParticleSpawner.cs
+++ Assets/Scripts/Mono Behaviours/ParticleSpawner.cs
@@ -12,15 +12,24 @@ namespace QFSW.GravityDOTS
 {
     public class ParticleSpawner : MonoBehaviour
     {
-        [SerializeField] private int _particleCount = 100;
-        [SerializeField] private float _particleMaxSpeed = 3;
+        [SerializeField]
+        private int _particleCount = 100;
 
-        [SerializeField] private float2 _particleMass = new float2(100, 1000);
-        [SerializeField] private float _particleDensity = 1;
+        [SerializeField]
+        private float _particleMaxSpeed = 3;
 
-        [SerializeField] private Material _particleMaterial;
-        [SerializeField] private Mesh _particleMesh;
-        
+        [SerializeField]
+        private float2 _particleMass = new float2(100, 1000);
+
+        [SerializeField]
+        private float _particleDensity = 1;
+
+        [SerializeField]
+        private Material _particleMaterial;
+
+        [SerializeField]
+        private Mesh _particleMesh;
+
         private EntityManager _entityManager;
         private EntityArchetype _particleType;
 
@@ -37,17 +46,15 @@ namespace QFSW.GravityDOTS
             _particleType = _entityManager.CreateArchetype(particleComponents);
 
             World.Active.GetOrCreateSystem<CollideMergeSystem>().ParticleDensity = _particleDensity;
-        }
 
-        private void Start()
-        {
             SpawnParticles(_particleCount);
         }
 
-        public void SpawnParticles(int count)
+        private void SpawnParticles(int count)
         {
-            LocalToWorld pos = new LocalToWorld { Value = float4x4.Translate(float3.zero) };
-            
+            float2 bottomLeft = (Vector2)Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+            float2 topRight = (Vector2)Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
             RenderMesh rm = new RenderMesh
             {
                 mesh = _particleMesh,
@@ -69,15 +76,24 @@ namespace QFSW.GravityDOTS
             for (int i = 0; i < particles.Length; i++)
             {
                 float mass = r.NextFloat(_particleMass.x, _particleMass.y);
-                float radius = math.pow(3 / (4 * math.PI) * mass / _particleDensity, 1 / 3f);
+                float radius = math.pow(3 / (4 * math.PI) * mass / _particleDensity, 1f / 3f);
+                float scale = radius * 2f;
+
+                LocalToWorld pos = new LocalToWorld
+                {
+                    Value = float4x4.TRS(new float3(r.NextFloat2(bottomLeft, topRight), 0f), quaternion.identity,
+                        new float3(scale, scale, scale))
+                };
 
                 Entity particle = particles[i];
-                _entityManager.SetComponentData(particle, pos);                        
+                _entityManager.SetComponentData(particle, pos);
                 _entityManager.SetComponentData(particle, bounds);
                 _entityManager.SetSharedComponentData(particle, rm);
+                _entityManager.SetComponentData(particle, new Mass() { Value = mass });
                 _entityManager.SetComponentData(particle, new Velocity() { Value = r.NextFloat2(minVal, maxVel) });
                 _entityManager.SetComponentData(particle, new Radius() { Value = radius });
-                _entityManager.SetComponentData(particle, new Scale() { Value = radius * 2f });
+                _entityManager.SetComponentData(particle, new Scale() { Value = scale });
+
             }
 
             particles.Dispose();
--- Assets/Scripts/Systems/CollideMergeSystem.cs
+++ Assets/Scripts/Systems/CollideMergeSystem.cs
@@ -15,9 +15,16 @@ namespace QFSW.GravityDOTS
 
 		private EntityQuery particleQuery;
 
+		private NativeHashMap<Entity, Entity> entitiesToDestroy;
+		private EntityCommandBufferSystem bufferSystem;
+
 		protected override void OnCreate()
 		{
+			bufferSystem = World.GetExistingSystem<EntityCommandBufferSystem>();
+			entitiesToDestroy = new NativeHashMap<Entity, Entity>(1024, Allocator.Persistent);
+
 			particleQuery = GetEntityQuery(
+				ComponentType.ReadOnly<LocalToWorld>(),
 				ComponentType.ReadOnly<Bounded>(),
 				typeof(Mass),
 				typeof(Radius),
@@ -25,99 +32,179 @@ namespace QFSW.GravityDOTS
 			);
 		}
 
-		private struct CollideMergeJob : IJobParallelFor
+		protected override void OnDestroy()
+		{
+			entitiesToDestroy.Dispose();
+		}
+
+		private struct CollideMergeJob : IJob
 		{
 			public float ParticleDensity;
 			public float DeltaTime;
 
+			[DeallocateOnJobCompletion]
 			public NativeArray<ArchetypeChunk> Chunks;
 
-			public ArchetypeChunkComponentType<Mass> ChunkMass;
-			public ArchetypeChunkComponentType<Radius> ChunkRadius;
-			public ArchetypeChunkComponentType<Scale> ChunkScale;
+			[ReadOnly]
+			public ArchetypeChunkEntityType EntityType;
+			
+			[ReadOnly]
+			public ArchetypeChunkComponentType<LocalToWorld> TransformType;
 
-			public ArchetypeChunkComponentType<Velocity> ChunkVelocity;
+			public ArchetypeChunkComponentType<Mass> MassType;
+			public ArchetypeChunkComponentType<Radius> RadiusType;
+			public ArchetypeChunkComponentType<Scale> ScaleType;
+
+			public ArchetypeChunkComponentType<Velocity> VelocityType;
+
+			public NativeHashMap<Entity, Entity> EntitiesToDestroy;
 
 			private void ExecuteChunks(ArchetypeChunk chunk1, ArchetypeChunk chunk2)
 			{
-				NativeArray<Mass> mass1 = chunk1.GetNativeArray(ChunkMass);
-				NativeArray<Radius> radius1 = chunk1.GetNativeArray(ChunkRadius);
-				NativeArray<Scale> scale1 = chunk1.GetNativeArray(ChunkScale);
+				NativeArray<LocalToWorld> transformData1 = chunk1.GetNativeArray(TransformType);
+				NativeArray<Mass> massData1 = chunk1.GetNativeArray(MassType);
+				NativeArray<Radius> radiusData1 = chunk1.GetNativeArray(RadiusType);
+				NativeArray<Scale> scaleData1 = chunk1.GetNativeArray(ScaleType);
+				NativeArray<Entity> entityData1 = chunk1.GetNativeArray(EntityType);
 
-				NativeArray<Mass> mass2 = chunk2.GetNativeArray(ChunkMass);
-				NativeArray<Radius> radius2 = chunk2.GetNativeArray(ChunkRadius);
-				
-				// instead of 4 separate cases
-				
-				// no velocity
-				// chunk1 has velocity, chunk2 does not
-				// chunk1 does not, chunk2 has velocity
-				// both have velocity
-				
-				// maybe reorder so that there are just 3 separate cases
-				
-				// no velocity
-				// a chunk has velocity, the other does not
-				// both have velocity
-				if (chunk1.Has(ChunkVelocity))
+				NativeArray<LocalToWorld> transformData2 = chunk2.GetNativeArray(TransformType);
+				NativeArray<Mass> massData2 = chunk2.GetNativeArray(MassType);
+				NativeArray<Radius> radiusData2 = chunk2.GetNativeArray(RadiusType);
+				NativeArray<Entity> entityData2 = chunk2.GetNativeArray(EntityType);
+
+				if (chunk1.Has(VelocityType))
 				{
-					NativeArray<Velocity> velocity1 = chunk1.GetNativeArray(ChunkVelocity);
+					NativeArray<Velocity> velocity1 = chunk1.GetNativeArray(VelocityType);
 
-					if (chunk2.Has(ChunkVelocity))
+					// both chunks have velocity
+					if (chunk2.Has(VelocityType))
 					{
-						NativeArray<Velocity> velocity2 = chunk2.GetNativeArray(ChunkVelocity);
+						NativeArray<Velocity> velocity2 = chunk2.GetNativeArray(VelocityType);
+
+						for (int i = 0; i < chunk1.Count; i++)
+						{
+							Entity current = entityData1[i];
+							if (EntitiesToDestroy.ContainsKey(current)) continue;
+
+							float3 pos1 = transformData1[i].Position;
+							float rad1 = radiusData1[i].Value;
+
+							for (int j = 0; j < chunk2.Count; j++)
+							{
+								Entity del = entityData2[j];
+								if (EntitiesToDestroy.ContainsKey(del)) continue;
+
+								float3 pos2 = transformData2[j].Position;
+								float rad = radiusData2[j].Value + rad1;
+
+								if (!(math.distancesq(pos1, pos2) < rad * rad)) continue;
+								
+								velocity1[i] = new Velocity
+									{ Value = (velocity1[i].Value + velocity2[j].Value) / 2f };
+								
+								float mass = massData1[i].Value + massData2[j].Value;
+								float newRadius = math.pow(3 / (4 * math.PI) * mass / ParticleDensity, 1f / 3f);
+								massData1[i] = new Mass { Value = mass };
+								radiusData1[i] = new Radius() { Value = newRadius };
+								scaleData1[i] = new Scale { Value = newRadius * 2f };
+
+								
+								EntitiesToDestroy.TryAdd(del, del);
+							}
+						}
 					}
-					else
+					else // chunk1 has velocity, chunk2 does not
 					{
-						
+						for (int i = 0; i < chunk1.Count; i++)
+						{
+							for (int j = 0; j < chunk2.Count; j++)
+							{
+
+							}
+						}
 					}
 				}
 				else
 				{
-					if (chunk2.Has(ChunkVelocity))
+					// chunk1 does not have velocity, chunk2 does
+					if (chunk2.Has(VelocityType))
 					{
-						NativeArray<Velocity> velocity2 = chunk2.GetNativeArray(ChunkVelocity);
+						NativeArray<Velocity> velocity2 = chunk2.GetNativeArray(VelocityType);
+
+						for (int i = 0; i < chunk1.Count; i++)
+						{
+							for (int j = 0; j < chunk2.Count; j++)
+							{
+
+							}
+						}
 					}
-					else
+					else // neither chunks have velocity
 					{
-						
+						for (int i = 0; i < chunk1.Count; i++)
+						{
+							for (int j = 0; j < chunk2.Count; j++)
+							{
+
+							}
+						}
 					}
 				}
 			}
 
-			public void Execute(int index)
+			public void Execute()
 			{
-				ArchetypeChunk chunk = Chunks[index];
-
 				for (int i = 0; i < Chunks.Length; i++)
 				{
-					ExecuteChunks(chunk, Chunks[i]);
+					ArchetypeChunk chunk = Chunks[i];
+
+					for (int j = 0; j < Chunks.Length; j++)
+					{
+						ExecuteChunks(chunk, Chunks[j]);
+					}
 				}
 			}
 		}
 
 		protected override JobHandle OnUpdate(JobHandle inputDeps)
 		{
-			ArchetypeChunkComponentType<Mass> chunkMass = GetArchetypeChunkComponentType<Mass>(false);
-			ArchetypeChunkComponentType<Radius> chunkRadius = GetArchetypeChunkComponentType<Radius>(false);
-			ArchetypeChunkComponentType<Scale> chunkScale = GetArchetypeChunkComponentType<Scale>(false);
+			EntityCommandBuffer buffer = bufferSystem.CreateCommandBuffer();
 
-			ArchetypeChunkComponentType<Velocity> chunkVelocity = GetArchetypeChunkComponentType<Velocity>(false);
+			using (NativeArray<Entity> arr = entitiesToDestroy.GetKeyArray(Allocator.Temp))
+			{
+				for (int i = 0; i < arr.Length; i++)
+				{
+					buffer.DestroyEntity(arr[i]);
+				}
+			}
 
 			NativeArray<ArchetypeChunk> chunks = particleQuery.CreateArchetypeChunkArray(Allocator.TempJob);
-			
+
+			ArchetypeChunkEntityType entityType = GetArchetypeChunkEntityType();
+			ArchetypeChunkComponentType<LocalToWorld> transformType = GetArchetypeChunkComponentType<LocalToWorld>(true);
+			ArchetypeChunkComponentType<Mass> massType = GetArchetypeChunkComponentType<Mass>(false);
+			ArchetypeChunkComponentType<Radius> radiusType = GetArchetypeChunkComponentType<Radius>(false);
+			ArchetypeChunkComponentType<Scale> scaleType = GetArchetypeChunkComponentType<Scale>(false);
+
+			ArchetypeChunkComponentType<Velocity> velocityType = GetArchetypeChunkComponentType<Velocity>(false);
+
+			entitiesToDestroy.Clear();
+
 			CollideMergeJob job = new CollideMergeJob
 			{
 				DeltaTime = Time.deltaTime,
 				ParticleDensity = ParticleDensity,
 				Chunks = chunks,
-				ChunkMass = chunkMass,
-				ChunkRadius = chunkRadius,
-				ChunkScale = chunkScale,
-				ChunkVelocity = chunkVelocity
+				EntityType = entityType,
+				TransformType = transformType,
+				MassType = massType,
+				RadiusType = radiusType,
+				ScaleType = scaleType,
+				VelocityType = velocityType,
+				EntitiesToDestroy = entitiesToDestroy,
 			};
-			
-			return job.Schedule(chunks.Length, 1, inputDeps);
+
+			return job.Schedule(inputDeps);
 		}
 	}
 }
