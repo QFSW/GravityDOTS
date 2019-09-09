8b1126c14c18405b928a289d9dcc0eede8b8d137
Miren <mirenbhakta21@gmail.com>
2019-09-09 10:56:36 -0500

    try to solve issues where main thread is waiting for jobs to complete

--- Assets/Scripts/Systems/CollideMergeSystem.cs
+++ Assets/Scripts/Systems/CollideMergeSystem.cs
@@ -21,13 +21,13 @@ namespace QFSW.GravityDOTS
 
 		protected override void OnCreate()
 		{
-			bufferSystem = World.GetExistingSystem<EntityCommandBufferSystem>();
+			bufferSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
 			entitiesToDestroy = new NativeHashMap<Entity, Entity>(1024, Allocator.Persistent);
 
 			particleQuery = GetEntityQuery(
 				ComponentType.ReadOnly<Bounded>(),
-                typeof(Translation),
-                typeof(Velocity),
+				typeof(Translation),
+				typeof(Velocity),
 				typeof(Mass),
 				typeof(Radius),
 				typeof(Scale)
@@ -43,6 +43,7 @@ namespace QFSW.GravityDOTS
 		{
 			public float ParticleDensity;
 
+			[ReadOnly, DeallocateOnJobCompletion]
 			public NativeArray<ArchetypeChunk> Chunks;
 
 			[ReadOnly]
@@ -59,7 +60,6 @@ namespace QFSW.GravityDOTS
 
 			[ReadOnly]
 			public ArchetypeChunkComponentType<Velocity> VelocityType;
-
 
 			public void Execute(Entity entity, int index, [ReadOnly] ref Translation pos, [ReadOnly] ref Velocity vel,
 				[ReadOnly] ref Mass mass, [ReadOnly] ref Radius rad)
@@ -76,91 +76,6 @@ namespace QFSW.GravityDOTS
 					for (int i = 0; i < chunk.Count; i++)
 					{
 
-					}
-				}
-			}
-		}
-
-		[BurstCompile]
-		private struct CollideMergeJobOld : IJob
-		{
-			public float ParticleDensity;
-
-			[DeallocateOnJobCompletion]
-			public NativeArray<ArchetypeChunk> Chunks;
-
-			[ReadOnly]
-			public ArchetypeChunkEntityType EntityType;
-
-			public ArchetypeChunkComponentType<Translation> PositionType;
-			public ArchetypeChunkComponentType<Mass> MassType;
-			public ArchetypeChunkComponentType<Radius> RadiusType;
-			public ArchetypeChunkComponentType<Scale> ScaleType;
-			public ArchetypeChunkComponentType<Velocity> VelocityType;
-
-			public NativeHashMap<Entity, Entity> EntitiesToDestroy;
-
-            private void ExecuteChunks(ArchetypeChunk chunk1, ArchetypeChunk chunk2)
-            {
-                NativeArray<Translation> transformData1 = chunk1.GetNativeArray(PositionType);
-                NativeArray<Mass> massData1 = chunk1.GetNativeArray(MassType);
-                NativeArray<Radius> radiusData1 = chunk1.GetNativeArray(RadiusType);
-                NativeArray<Scale> scaleData1 = chunk1.GetNativeArray(ScaleType);
-                NativeArray<Entity> entityData1 = chunk1.GetNativeArray(EntityType);
-                NativeArray<Velocity> velocity1 = chunk1.GetNativeArray(VelocityType);
-
-                NativeArray<Translation> transformData2 = chunk2.GetNativeArray(PositionType);
-                NativeArray<Mass> massData2 = chunk2.GetNativeArray(MassType);
-                NativeArray<Radius> radiusData2 = chunk2.GetNativeArray(RadiusType);
-                NativeArray<Entity> entityData2 = chunk2.GetNativeArray(EntityType);
-                NativeArray<Velocity> velocity2 = chunk2.GetNativeArray(VelocityType);
-
-                for (int i = 0; i < chunk1.Count; i++)
-                {
-                    Entity current = entityData1[i];
-                    if (EntitiesToDestroy.ContainsKey(current)) continue;
-
-                    float3 pos1 = transformData1[i].Value;
-                    float rad1 = radiusData1[i].Value;
-
-                    for (int j = 0; j < chunk2.Count; j++)
-                    {
-                        Entity del = entityData2[j];
-                        if (current == del) continue;
-                        if (EntitiesToDestroy.ContainsKey(del)) continue;
-
-                        float3 pos2 = transformData2[j].Value;
-                        float rad = radiusData2[j].Value + rad1;
-
-                        float distance = math.distancesq(pos1, pos2);
-                        if (distance < rad * rad)
-                        {
-                            float mass = massData1[i].Value + massData2[j].Value;
-                            float newRadius = math.pow(3 / (4 * math.PI) * mass / ParticleDensity, 1f / 3f);
-                            float2 newVelocity = (velocity1[i].Value * massData1[i].Value + velocity2[j].Value * massData2[j].Value) / mass;
-                            float3 newPos = (pos1 * massData1[i].Value + pos2 * massData2[j].Value) / mass;
-
-                            massData1[i] = new Mass { Value = mass };
-                            velocity1[i] = new Velocity { Value = newVelocity };
-                            transformData1[i] = new Translation { Value = newPos };
-                            radiusData1[i] = new Radius() { Value = newRadius };
-                            scaleData1[i] = new Scale { Value = newRadius * 2f };
-
-                            EntitiesToDestroy.TryAdd(del, del);
-                        }
-                    }
-                }
-            }
-
-			public void Execute()
-			{
-				for (int i = 0; i < Chunks.Length; i++)
-				{
-					ArchetypeChunk chunk = Chunks[i];
-
-					for (int j = 0; j < Chunks.Length; j++)
-					{
-						ExecuteChunks(chunk, Chunks[j]);
 					}
 				}
 			}
@@ -184,12 +99,11 @@ namespace QFSW.GravityDOTS
 			ArchetypeChunkComponentType<Translation> transformType = GetArchetypeChunkComponentType<Translation>(true);
 			ArchetypeChunkComponentType<Mass> massType = GetArchetypeChunkComponentType<Mass>(true);
 			ArchetypeChunkComponentType<Radius> radiusType = GetArchetypeChunkComponentType<Radius>(true);
-			ArchetypeChunkComponentType<Scale> scaleType = GetArchetypeChunkComponentType<Scale>(true);
 			ArchetypeChunkComponentType<Velocity> velocityType = GetArchetypeChunkComponentType<Velocity>(true);
 
 			entitiesToDestroy.Clear();
 
-			CollideMergeJob jobOld = new CollideMergeJob
+			CollideMergeJob job = new CollideMergeJob
 			{
 				ParticleDensity = ParticleDensity,
 				Chunks = chunks,
@@ -200,7 +114,9 @@ namespace QFSW.GravityDOTS
 				VelocityType = velocityType,
 			};
 
-			return jobOld.Schedule(this, inputDeps);
+			JobHandle handle = job.Schedule(this, inputDeps);
+			bufferSystem.AddJobHandleForProducer(handle);
+			return handle;
 		}
 	}
 }
